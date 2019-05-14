0b08aeb6a6b4b627c10d8298938882e0b98f0dce
onewoorks <irwanbibrahim@onewoorks.com>
2019-05-14 11:48:27 +0800

    router set

---
+++ app/controllers/cdccms.js
@@ -0,0 +1,126 @@
+const logger = require('../templates/logger')
+const fact_fulfilment = require('../models/cdccms/fact_fulfilment')
+const moment = require('moment')
+const fs = require('fs')
+
+module.exports = {
+    fulfilment: (data) => {
+        logger.accept_message(data)
+        //    sample_feeder.fulfilment(data)
+        // fact_fulfilment.fulfilment_tables()
+        _clear_fact_with_current_year()
+        _check_fulfilment_tables( (results) => {
+            for(const result of results.summary){
+                _read_fulfilment_table(result)
+            } 
+        })
+    },
+    fulfilment_current: (data) =>{
+        _clear_fact_with_current_year()
+        let table = {
+            table_name: 'ep_fulfilment'
+        }
+        _read_fulfilment_table(table)
+    }
+
+}
+
+_return_info = (x) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(x)
+        }, 1000)
+    })
+}
+
+_clear_fact_with_current_year = () => {
+    fact_fulfilment.delete_current_year_fulfilment()
+}
+
+_check_fulfilment_tables = async (callback) => {
+    fact_fulfilment.fulfilment_tables(async results => {
+        logger.wait_message('Checking fulfilment tables information')
+        var table_fulfilment = []
+        for (const result of results) {
+            const r = await _return_info(result)
+            var a = await new Promise((resolve, reject) => {
+                fact_fulfilment.count_data(r.tables, (result) => {
+                    resolve({
+                        table_name: r.tables,
+                        rows: result.total_count
+                    })
+                })
+            }).then(response => response)
+            let summary = {
+                table_name: r.tables,
+                rows: a.rows
+            }
+            table_fulfilment.push(summary)
+        }
+        let data = { 
+            table_category: 'ep_fulfilment',
+            summary: table_fulfilment
+        }
+        callback(data)
+        logger.result_message(data)
+    })
+}
+
+_read_fulfilment_table = (table) => {
+    fact_fulfilment.fulfilment_data( table, result => {
+        _create_temp_csv_file(table.table_name, result)
+    })
+}
+
+_create_temp_csv_file = async (table_name, data) => {
+    let list = []
+    var header = Object.keys(data[0])
+    list.push(header)
+    for(const d of data){
+        let r = []
+        header.forEach(value=>{
+            r.push(d[value])
+        })
+        list.push(r)
+    }
+    await _writingCsv(table_name, list)
+}
+
+_writingCsv = (table_name, list_data) => {
+    var lists = []
+    list_data.forEach((value, key) => {
+        if(key>0){
+            list_data[key].shift()
+        }
+        lists.push(list_data[key] + '\r\n')
+    }) 
+    let filename = `./csvs/${moment().unix()}_${table_name}.csv`
+    logger.wait_message("Script is trying to save a temporary file...")
+    fs.writeFile(filename, lists, 'utf8', function (err) {
+        let msg = {}
+        if (err) {
+            msg = {
+                message: "Error occured!",
+                description: err
+            }
+            logger.result_message(msg)
+        } else {
+            msg = {
+                message: "Temporary file has been created!",
+                filename: filename,
+            }
+            logger.result_message(msg)
+            let info = {
+                filename: filename,
+                table_name: 'fact_fulfilment'
+            }
+            fact_fulfilment.uploadLoadInFile(info, result => {
+                logger.result_message(result)
+                fs.unlinkSync(result.filename)
+            
+            })
+        }
+        
+    });
+
+}
---
+++ app/models/cdccms/fact_fulfilment.js
@@ -0,0 +1,70 @@
+const db = require('../../configs/databases')
+const path = require('path')
+const db_name = path.basename(path.dirname(__filename))
+
+const fulfilment_tables = (callback) => {
+    var query = `SELECT TABLE_NAME AS tables 
+        FROM INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_SCHEMA = 'sample_feeder'
+        AND TABLE_NAME LIKE 'ep_fulfilment%'`;
+    db[db_name].query(query, (error, rows, fields) => {
+        if (!error) {
+            return callback(JSON.parse(JSON.stringify(rows)))
+        }
+    })
+}
+
+module.exports = {
+    fulfilment_tables,
+    count_data: (table, callback) => {
+        var query = `SELECT count(*) as total_count FROM ${table}`
+        db[db_name].query(query, (err, rows, fields) => {
+            return !err ? callback(db.return_result(rows[0])) : err
+        })
+    },
+    fulfilment_data: (table, callback) => {
+        var query = `SELECT * FROM ${table.table_name}`
+        db[db_name].query(query, (err, rows, fields) => {
+            return !err ? callback(db.return_result(rows)) : err
+        })
+    },
+    fulfilment_current_year: (callback) => {
+        var query = "SELECT * FROM ep_fulfilment"
+        db[db_name].query(query, (err, rows, fields)=>{
+            return (!err) ? callback(rows) : err
+        })
+    },
+    delete_current_year_fulfilment: () => {
+        var thisYear = new Date().getFullYear()
+        var query = `DELETE FROM fact_fulfilment WHERE YEAR(fl_created_date) = ${thisYear}`
+        db[db_name].query(query, (err, rows, fields)=>{
+            if (!err) console.log(`${thisYear} - fulfilment data is deleted!\r\n`)
+        })  
+    },
+
+    check_current_fact_table: (table, callback) => {
+        var query = `SELECT count(*)
+        AS total,
+        fl_created_date AS year
+        FROM fact_fulfilment
+        GROUP BY YEAR(fl_created_date)`
+        db[db_name].query(query, (err, rows, fields) => {
+            return !err ? callback(db.return_result(rows)) : err
+        })
+    },
+
+    uploadLoadInFile: async (info, callback) => {
+        var query = `LOAD DATA LOCAL INFILE '${info.filename}' `
+        query += `INTO TABLE ${info.table_name} `
+        query += "FIELDS TERMINATED BY ',' "
+        query += `ENCLOSED BY '"' `
+        query += "LINES TERMINATED BY '\r\n' "
+        query += "IGNORE 1 ROWS;"
+        await db[db_name].query(query, (error, rows, next) => {
+            return  (!error) ? callback({
+                status:"success",
+                filename: info.filename
+            }) : error
+        })
+    }
+}
